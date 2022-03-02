81897e2da86c38da5e86d9aaff8eb005115767eb
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-03-02 01:14:33 -0500

    Add G analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,16 @@ export default function Document() {
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
         <link rel="shortcut icon" href="/favicon.ico" />
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FNZ9GY0WPZ"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-FNZ9GY0WPZ');`,
+          }}
+        />
       </Head>
       <body>
         <Main />
