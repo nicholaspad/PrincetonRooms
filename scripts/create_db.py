2f288c47da14c8d4da9442a459d25a87fb15c8b3
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-03-28 16:51:24 -0400

    Remove old code

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -1,6 +1,4 @@
-import os
 import json
-import pandas as pd
 import pymongo
 from datetime import datetime
 
@@ -53,36 +51,6 @@ def create_db():
                         entry["building_name"] = building["name"]
 
         db.reviews.insert_one(entry)
-        print(f"inserted review #{id}")
-        id += 1
-
-    return
-
-    data = pd.read_csv("cleaned.csv")
-    id = 0
-    for _, row in data.iterrows():
-        new_review = {
-            "_id": id,
-            "date": datetime.strptime(row["date"], "%Y-%m-%d"),
-            "content": row["content"],
-            "rating": int(row["rating"]),
-            "building_name": row["name"],
-            "room_number": row["number"],
-            "room_sqft": int(row["sqft"]),
-            "room_occ": int(row["occ"]),
-            "room_subfree": bool(row["subfree"]),
-            "room_numrooms": int(row["numrooms"]),
-            "room_floor": int(row["floor"]),
-        }
-
-        if row["bathroom"] == "SH":
-            new_review["room_bathroomtype"] = "shared"
-        elif row["bathroom"] == "PR":
-            new_review["room_bathroomtype"] = "private"
-        else:
-            new_review["room_bathroomtype"] = "public"
-
-        db.reviews.insert_one(new_review)
         print(f"inserted review #{id}")
         id += 1
 
